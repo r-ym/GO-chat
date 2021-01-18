962ba96d653c105b36ec0a693d7d671dd8c2fd70
r-ym <ramanyachi@gmail.com>
2021-01-18 15:53:27 +0900

    yes

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import './App.css';
+import { connect, sendMsg } from "./api";
+import Header from './components/Header/Header';
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    connect();
+  }
+
+  send() {
+    console.log("hello");
+    sendMsg("hello");
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <Header />
+      <button onClick={this.send}>Hit</button>
+    </div>
+    );
+  }
+}
+
+export default App;
